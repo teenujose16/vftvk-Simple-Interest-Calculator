2ba22c332e877ba50139c6bed79ddfa5ae00e57e
Teenu Jose <teenujose16@gmail.com>
2021-04-10 23:24:04 +0530

    Textbox validation and result

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-function compute(principal, rate, years)
-{
-    principal = document.getElementById("principal").value;
-    rate = document.getElementById("rate").value;
-    years = document.getElementById("years").value;
+ function compute(principal, interestRate, years) {
    
-    var interest = document.getElementById("result") ;
-    interest.innerHTML = "The interest is " + (principal*rate*years/100);
-    
-    var totalAmount = principal + interest;
-    var totalYear = 2021 + years
-    
-     document.getElementById("p01").innerHTML = "If you deposit " + principal;
-     document.getElementById("p02").innerHTML = "at an interest rate of " + rate;                     
- 	 document.getElementById("p03").innerHTML = "You will receive an amount of " + totalAmount;		
-	 document.getElementById("p04").innerHTML = "in the year " + totalYear;			
- }
+				
+          document.getElementById("divContainer").style.display = "block";
+          
+          //assigning value variables
+          principal = document.getElementById("principalId").value;
+          interestRate = document.getElementById("rateId").value;
+          years = document.getElementById("yearsId").value;
+          
+          //formulate result 
+          var resultInterest = (principal * interestRate * years / 100);
+          var totalAmount =  parseInt(principal) + parseInt(resultInterest);
+          var totalYear = 2021 + parseInt(years);
+           
+           //assigning value in textbox text
+          document.getElementById("principalOutput").innerHTML =  principal;
+          document.getElementById("interestOutput").innerHTML = interestRate;
+          document.getElementById("totalamountOutput").innerHTML =  totalAmount;
+          document.getElementById("yearOutput").innerHTML =  totalYear;
+          
  
- function hidePara()
- {
-        document.getElementById('p01').style.display = "block";
-		document.getElementById('p02').style.display = "block";
- 		document.getElementById('p03').style.display = "block";
-		document.getElementById('p04').style.display = "block"
-	
-		
- }       
+        }
+function hideDetails()
+{
+       //hide output display
+      document.getElementById("divContainer").style.display = "none";
+
+}
+
+function validate()
+{
+
+        //validating textbox for principal amount
+       
+         principal = document.getElementById("principalId").value
+          console.log(principal);
+   
+  // checking the input text must not be zero or negative
+         if (principal == "" || principal == 0 || principal < 0) {
+           
+              document.getElementById("divContainer").style.display = "none";
+              console.log("validatingNull");
+              window.alert("Enter a positive number");
+              //focus used for take the user back to input box
+              document.getElementById("principalId").focus();
+            return false;
+        }
+       
+    }
