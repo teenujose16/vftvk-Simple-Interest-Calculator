315b3bbd202e6c066b79a77c10afae7f6d55dabe
Teenu Jose <teenujose16@gmail.com>
2021-04-20 08:18:28 +0530

    final modification oj project

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
  function compute(principal, interestRate, years) {
-   
-				
+    
+    principal = document.getElementById("principalId").value;
+    if (principal == "" || principal == 0 || principal < 0) { 
+       
+              document.getElementById("divContainer").style.display = "none";
+              console.log("validatingNull");
+              window.alert("Enter a positive number");
+              //focus used for take the user back to input box
+              document.getElementById("principalId").focus();
+            
+        }
+       else
+       {
+       
           document.getElementById("divContainer").style.display = "block";
-          
-          //assigning value variables
+           //assigning value variables
           principal = document.getElementById("principalId").value;
           interestRate = document.getElementById("rateId").value;
           years = document.getElementById("yearsId").value;
           
           //formulate result 
           var resultInterest = (principal * interestRate * years / 100);
-          var totalAmount =  parseInt(principal) + parseInt(resultInterest);
-          var totalYear = 2021 + parseInt(years);
+          var totalAmount =  parseInt(principal) + parseInt(resultInterest); 
+          var dateobj = new Date();
+            // Year from above object 
+            // is being fetched using getFullYear() 
+           var dateObject = dateobj.getFullYear();
+          
+            var totalYear = dateObject + parseInt(years);
            
            //assigning value in textbox text
           document.getElementById("principalOutput").innerHTML =  principal;
@@ -19,32 +35,12 @@
           document.getElementById("totalamountOutput").innerHTML =  totalAmount;
           document.getElementById("yearOutput").innerHTML =  totalYear;
           
- 
-        }
+       }
+  }
+  
 function hideDetails()
 {
        //hide output display
       document.getElementById("divContainer").style.display = "none";
 
 }
-
-function validate()
-{
-
-        //validating textbox for principal amount
-       
-         principal = document.getElementById("principalId").value
-          console.log(principal);
-   
-  // checking the input text must not be zero or negative
-         if (principal == "" || principal == 0 || principal < 0) {
-           
-              document.getElementById("divContainer").style.display = "none";
-              console.log("validatingNull");
-              window.alert("Enter a positive number");
-              //focus used for take the user back to input box
-              document.getElementById("principalId").focus();
-            return false;
-        }
-       
-    }
